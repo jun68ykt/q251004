e90e538c2fd909f59d267a63e79ae3dc6fb0828b
Your Name <you@example.com>
2020-04-04 09:58:33 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# TL;DR
+
+This repo contains sample codes referred to [my answer](https://teratail.com/questions/251004#reply-363096) for the [question No.251004](https://teratail.com/questions/251004 ) of [teratail](https://teratail.com/), which is a Programming Q&A site of Japan.
+
+# How to install and run
+
+- yarn
+- node index.js
---
+++ index.js
@@ -0,0 +1,15 @@
+const [fs, { JSDOM }, jquery] = [require('fs'), require('jsdom'), require('jquery')];
+
+// JQuery オブジェクトからHTML文字列を得るための関数
+const htmlStr = jq => jq.clone().wrap('<div/>').parent().html();
+
+// ファイル test.html を読み込み、JQuery オブジェクト $ を作成
+const html = fs.readFileSync('test.html', 'utf-8'),
+      dom = new JSDOM(html),
+      $ = jquery(dom.window);
+
+// scriptタグを収集して、各々をHTML文字列にして表示
+$('script').each(function() {
+  console.log(htmlStr($(this)));
+  console.log("--------------");
+});
